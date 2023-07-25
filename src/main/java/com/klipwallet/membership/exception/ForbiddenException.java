8850cb28152fe49da48c8988f115080d781993e1
jordan.jung <jordan.jung@groundx.xyz>
2023-07-26 08:49:18 +0900

    [NO-TASK] Build a code base(2)
    - Add error code & exceptions
    - Kmt -> KlipMembership
    - Etc

--- src/main/java/com/klipwallet/membership/exception/ForbiddenException.java
+++ src/main/java/com/klipwallet/membership/exception/ForbiddenException.java
@@ -1,25 +1,24 @@
 package com.klipwallet.membership.exception;
 
+import lombok.Getter;
+
 import com.klipwallet.membership.entity.AuthenticatedUser;
 
 /**
- * <b>기본 권한 부족 예외</b>
+ * <b>기본 권한 부족 예외. {@code error.forbidden}</b>
  * <p>
  * 일반적으로 {@literal http}로 응답시 {@code Status 403}을 반환한다.
  * </p>
  */
-public class ForbiddenException extends BaseException {
+public class ForbiddenException extends BaseMessageException {
     /**
-     * 권한이 부족한 멤버 객체.
+     * 권한이 부족한 User 객체.
      */
-    private final AuthenticatedUser member;
+    @Getter
+    private final AuthenticatedUser user;
 
-    public ForbiddenException(AuthenticatedUser member) {
-        super();
-        this.member = member;
-    }
-
-    public AuthenticatedUser getMember() {
-        return member;
+    public ForbiddenException(AuthenticatedUser user) {
+        super("error.forbidden");
+        this.user = user;
     }
 }
