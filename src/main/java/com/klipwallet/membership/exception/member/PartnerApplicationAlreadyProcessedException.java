fcc59a3807735d55373ffa29a6642fb8b75f597b
winnie.byun <winnie.byun@groundx.xyz>
2023-08-17 15:12:30 +0900

    add error code properly

--- src/main/java/com/klipwallet/membership/exception/member/PartnerApplicationAlreadyProcessedException.java
+++ src/main/java/com/klipwallet/membership/exception/member/PartnerApplicationAlreadyProcessedException.java
@@ -9,9 +9,7 @@ import com.klipwallet.membership.exception.InvalidRequestException;
 
 public class PartnerApplicationAlreadyProcessedException extends InvalidRequestException {
     public PartnerApplicationAlreadyProcessedException(AppliedPartner appliedPartner) {
-        super(ErrorCode.INVALID_REQUEST_ACCEPT_ALREADY_PROCESSED,
-              "ID %d에 대한 정보는 이미 처리되었습니다. (처리 상태 : %s, 처리자 : %s, 처리 시각: %s)".formatted(appliedPartner.getId(), appliedPartner.getStatus(),
-                                                                                      appliedPartner.getUpdatedBy(),
-                                                                                      appliedPartner.getUpdatedAt().toString()));
+        super(ErrorCode.INVALID_REQUEST_ACCEPT_ALREADY_PROCESSED, appliedPartner.getId(), appliedPartner.getStatus(), appliedPartner.getUpdatedBy(),
+              appliedPartner.getUpdatedAt().toString());
     }
 }
--- src/main/java/com/klipwallet/membership/exception/member/PartnerNotFoundException.java
+++ src/main/java/com/klipwallet/membership/exception/member/PartnerNotFoundException.java
@@ -5,6 +5,6 @@ import com.klipwallet.membership.exception.NotFoundException;
 
 public class PartnerNotFoundException extends NotFoundException {
     public PartnerNotFoundException(Integer id) {
-        super(ErrorCode.PARTNER_NOT_FOUND, "ID %d에 대한 파트너 정보를 조회할 수 없습니다.".formatted(id));
+        super(ErrorCode.PARTNER_NOT_FOUND, id);
     }
 }
