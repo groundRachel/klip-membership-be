7ed7687216021f3d3a43db74ab64e39d0be94541
jordan.jung <jordan.jung@groundx.xyz>
2023-08-18 16:07:58 +0900

    KLDV-3125 [Membership] Admin > 공지사항 > 공지사항 목록/상세 조회 - 공지 상태 변경 API 추가 - Notice 리팩토링

--- src/main/java/com/klipwallet/membership/entity/Notice.java
+++ src/main/java/com/klipwallet/membership/entity/Notice.java
@@ -1,5 +1,7 @@
 package com.klipwallet.membership.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -8,6 +10,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
@@ -15,6 +20,7 @@ import lombok.ToString;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.lang.Nullable;
 
 import com.klipwallet.membership.adaptor.jpa.ForJpa;
 
@@ -23,6 +29,7 @@ import static java.lang.Boolean.TRUE;
 /**
  * 공지사항 Entity
  */
+@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @DynamicUpdate
@@ -38,13 +45,28 @@ public class Notice extends AbstractAggregateRoot<ChatRoom> {
     @Column(nullable = false)
     private String body;
     /**
-     * 메인 공지 노출 여부
+     * 고정 공지 여부
      * <p>
-     * 메인 공지는 모든 공지사항 중 단 하나만 선정될 수 있음.
+     * 고정 공지는 모든 공지사항 중 단 하나만 선정될 수 있으며, 메인에 노출된다.
      * </p>
+     * 칼럼명을 {@code is_primary}로 설정한 이유는 {@code primary}가 예약어
      */
-    @Column(nullable = false)
-    private boolean main = false;
+    @Column(name = "is_primary", nullable = false)
+    private boolean primary = false;
+    /**
+     * <pre>draft, live, inactive</pre>
+     */
+    private Status status;
+
+    /**
+     * 발행일시
+     * <p>
+     * 최근 발행일시? 최초 발행일시?
+     * </p>
+     *
+     * @see Status#LIVE
+     */
+    private LocalDateTime livedAt;
 
     @Embedded
     private BaseMeta base;
@@ -53,48 +75,112 @@ public class Notice extends AbstractAggregateRoot<ChatRoom> {
     protected Notice() {
     }
 
+    /**
+     * 기본 생성자.
+     *
+     * @param title     제목
+     * @param body      본문
+     * @param createdBy 생성자
+     */
     public Notice(String title, String body, MemberId createdBy) {
         this.title = title;
         this.body = body;
+        this.status = Status.DRAFT;
         this.base = new BaseMeta(createdBy);
     }
 
     /**
-     * 메인 공지 활성화
+     * 고정 공지로 설정
      */
-    private void activateMain() {
-        if (this.main) {    // 멱등성
+    private void primaryOn() {
+        if (this.primary) {    // 멱등성
             return;
         }
-        this.main = true;
-        this.registerEvent(new MainNoticeActivated(this.getId()));
+        this.primary = true;
+        this.registerEvent(new PrimaryNoticeChanged(this.getId()));
     }
 
     /**
-     * 메인 공지 비활성화
+     * 고정 공지 끄기
      */
-    public void deactivateMain() {
-        this.main = false;
+    public void primaryOff() {
+        this.primary = false;
     }
 
     public void update(NoticeUpdatable command) {
         this.title = command.getTitle();
         this.body = command.getBody();
-        changeMain(command.isMain());
+        changePrimary(command.isPrimary());
     }
 
-    private void changeMain(Boolean main) {
-        if (main == null) {
+    private void changePrimary(Boolean isPrimary) {
+        if (isPrimary == null) {
             return;
         }
-        if (TRUE.equals(main)) {    // 메인 공지 여부인 경우
-            this.activateMain();
+        if (TRUE.equals(isPrimary)) {    // 메인 공지 여부인 경우
+            this.primaryOn();
             return;
         }
-        this.deactivateMain();
+        this.primaryOff();
+    }
+
+    public void changeStatus(@NonNull Status status, MemberId updater) {
+        if (this.status == status) {
+            return;
+        }
+        updatedBy(updater);
+        if (status == Status.LIVE) {
+            live();
+        } else {
+            this.status = status;
+        }
+    }
+
+    private void updatedBy(MemberId updater) {
+        this.base = this.base.withUpdatedBy(updater);
+    }
+
+    private void live() {
+        this.status = Status.LIVE;
+        this.livedAt = LocalDateTime.now();
     }
 
     public boolean equalId(@NonNull Integer noticeId) {
         return noticeId.equals(this.id);
+    }
+
+    @Getter
+    @Schema(name = "Notice.Status", description = "공지사항 상태", example = "live")
+    public enum Status implements Statusable {
+        /**
+         * 초안: 최초 등록 후 발행 전까지 상태. 이용자가 조회할 수 없음.
+         */
+        DRAFT(0),
+        /**
+         * 발행: 이용자가 조회할 수 있음.
+         */
+        LIVE(1),
+        /**
+         * 비활성화: 이용자가 조회할 수 없음.
+         */
+        INACTIVE(2);
+
+        private final byte code;
+
+        Status(int code) {
+            this.code = Statusable.requireVerifiedCode(code);
+        }
+
+        @JsonCreator
+        @Nullable
+        public static Status fromDisplay(String display) {
+            return Statusable.fromDisplay(Status.class, display);
+        }
+
+        @JsonValue
+        @Override
+        public String toDisplay() {
+            return Statusable.super.toDisplay();
+        }
     }
 }
