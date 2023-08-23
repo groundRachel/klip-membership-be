a736a5bd62526353aedc04e39ca92e57570ef973
jordan.jung <jordan.jung@groundx.xyz>
2023-08-23 16:56:14 +0900

    KLDV-3079 Etc > 개발 구성 > FileUpload - javadoc 강화

--- src/main/java/com/klipwallet/membership/repository/AttachFile.java
+++ src/main/java/com/klipwallet/membership/repository/AttachFile.java
@@ -35,16 +35,38 @@ public class AttachFile extends BaseEntity<AttachFile> {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
+    /**
+     * 원본 파일명(거의 사용할 일이 없으나, 파일 다운로드가 추후 구현된다면 사용될 수 있음.
+     */
     private String filename;
     @Column(nullable = false)
     private MediaType contentType;
+    /**
+     * 파일 byte 사이즈
+     */
     @Column(nullable = false)
     private Long contentLength;
+    /**
+     * 스토리지 서비스(ex: S3)의 ObjectId
+     */
     @Embedded
     private ObjectId objectId;
+    /**
+     * 업로드 된 파일에 접근 가능한 실제 URL
+     * <pre>
+     * ex: {@code https://media.klipwallet.com/klip-membership/3/228492df-e771-43cb-b22e-1aba918c98ef}
+     * </pre>
+     */
     @Column(nullable = false)
     private String linkUrl;
+    /**
+     * 파일 연결 상태
+     * <p>
+     * UNLINK: 공지 사항 본문에 해당 파일이 링크되어 있지만 관계가 없어서 확인할 방도가 없음 = <b>고아 객체</b><br/>
+     * LINKED: 공지 사항에서 해당 파일을 사용하고 있으면 Notice-AttachFile 관계 Entity를 통해서 AttachFile Entity를 참조하고 있음.
+     * 만약 LINKED 상태라면 공지사항 삭제 시 Notice-AttachFile 관계 Entity를 통해서 첨부파일도 같이 삭제할 수 있음.
+     * </p>
+     */
     @Column(nullable = false)
     private LinkStatus linkStatus;
 
