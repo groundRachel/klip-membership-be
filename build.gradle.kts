a8c6452c7451c010ce79deebe2c18e496409c89a
Ian Han <jisu.han3201@gmail.com>
2023-08-24 14:42:20 +0900

    Apply jordan's review

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,9 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.cloud:spring-cloud-starter-vault-config")
+    implementation(platform("software.amazon.awssdk:bom:2.20.131"))
+    implementation("software.amazon.awssdk:s3")
+    implementation("software.amazon.awssdk:cloudfront")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("io.lettuce:lettuce-core")    // Redis
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springdocVersion")}")
