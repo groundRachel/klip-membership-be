a12495548b121e921626a632f5f3708164147c90
JiEun Lee <rachel.lee@groundx.xyz>
2023-08-25 15:57:15 +0900

    add response requiredMode

--- src/main/java/com/klipwallet/membership/dto/member/MemberSummary.java
+++ src/main/java/com/klipwallet/membership/dto/member/MemberSummary.java
@@ -6,9 +6,11 @@ import lombok.NonNull;
 
 import com.klipwallet.membership.entity.MemberId;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 @Schema(description = "멤버 요약 정보", accessMode = AccessMode.READ_ONLY)
-public record MemberSummary(@Schema(description = "멤버 아이디", type = "int32", example = "1") @NonNull MemberId id,
-                            @Schema(description = "멤버 이름", example = "정조던") @NonNull String name) {
+public record MemberSummary(@Schema(description = "멤버 아이디", requiredMode = REQUIRED, type = "int32", example = "1") @NonNull MemberId id,
+                            @Schema(description = "멤버 이름", requiredMode = REQUIRED, example = "정조던") @NonNull String name) {
     public static MemberSummary deactivated(MemberId memberId) {
         // TODO FIXME
         return new MemberSummary(memberId, "Deactivated");
