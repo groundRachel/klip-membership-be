6c2907daf6ecc6267f0c6532587d8d084f34049a
winnie.byun <winnie.byun@groundx.xyz>
2023-08-29 11:22:53 +0900

    add comment

--- src/main/java/com/klipwallet/membership/entity/PartnerApplicationRejected.java
+++ src/main/java/com/klipwallet/membership/entity/PartnerApplicationRejected.java
@@ -3,6 +3,11 @@ package com.klipwallet.membership.entity;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+/**
+ * 파트너 신청 거절됨 DomainEvent
+ *
+ * @see com.klipwallet.membership.entity.PartnerApplication#reject(String, MemberId)
+ */
 @Value
 @EqualsAndHashCode(callSuper = false)
 public class PartnerApplicationRejected extends DomainEvent {
