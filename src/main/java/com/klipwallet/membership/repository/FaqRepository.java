24969ddc86d5b0c5242fc6b3c64dab6e83d45783
JiEun Lee <rachel.lee@groundx.xyz>
2023-08-30 12:27:31 +0900

    apply feedback from jordan (findAll -> findByStatusNot)

--- src/main/java/com/klipwallet/membership/repository/FaqRepository.java
+++ src/main/java/com/klipwallet/membership/repository/FaqRepository.java
@@ -1,5 +1,7 @@
 package com.klipwallet.membership.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,11 +13,11 @@ public interface FaqRepository extends AbstractArticleRepository<Faq> {
     /**
      * 특정 상태를 제외한 정렬된 FAQ 목록 조회
      *
-     * @param status   검색에서 제외할 상태
+     * @param statuses   검색될 상태들
      * @param pageable page, size, sort
      * @return 특정 상태를 제외하여 검색한 정렬된 FAQ 목록
      */
-    Page<Faq> findByStatusNot(ArticleStatus status, Pageable pageable);
+    Page<Faq> findAllByStatusIn(Collection<ArticleStatus> statuses, Pageable pageable);
 
     /**
      * 상태 검색한 정렬된 FAQ 목록 조회
