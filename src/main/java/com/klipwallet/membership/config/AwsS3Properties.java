de66f2ac47d6fb21d5ddfe1b37b8b4e2bb8da1a5
winnie.byun <winnie.byun@groundx.xyz>
2023-09-04 15:05:19 +0900

    feature/AwsS3Properties-fix fix validation annotations in AwsS3Properties

--- src/main/java/com/klipwallet/membership/config/AwsS3Properties.java
+++ src/main/java/com/klipwallet/membership/config/AwsS3Properties.java
@@ -5,15 +5,19 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
 
 @Value
 @ConfigurationProperties(prefix = "cloud.aws.s3")
+@Validated
 public class AwsS3Properties {
+    @NotEmpty
     String bucket;
+    @NotEmpty
     String prefix;
 
     @ConstructorBinding
-    public AwsS3Properties(@NotEmpty String bucket, @NotEmpty String prefix) {
+    public AwsS3Properties(String bucket, String prefix) {
         this.bucket = bucket;
         this.prefix = prefix;
     }
